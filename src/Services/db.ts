3619a07fd9e78811a5337852cac5365b57749d81
TIYASHA BANERJEE <tiyasha@medius.in>
2021-11-27 23:10:31 +0530

    My First Commit

--- src/Services/db.ts
+++ src/Services/db.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const db_path: string = <string>process.env.DB_PATH;
 mongoose.connect(db_path, () => {
-  console.log("Connected to dabase");
+  console.log("Connected to MongoDBdatabase");
 });
 
-module.exports = { dbConnection: mongoose };
+module.exports = { dbConnection: mongoose }; 
