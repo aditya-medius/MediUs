88c703fa7b72443f2cfe13b7bda8c2d1548637af
TIYASHA BANERJEE <tiyasha@medius.in>
2021-12-25 15:11:27 +0530

    View Appointments

--- dist/Models/Appointment.Model.js
+++ dist/Models/Appointment.Model.js
@@ -47,7 +47,7 @@ const appointmentSchema = new mongoose_1.Schema({
                         // Time of day, like 1AM, 12PM, 10PM
                         enum: [
                             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-                            19, 20, 21, 22, 23, 24,
+                            19, 20, 21, 22, 23,
                         ],
                     },
                 },
@@ -60,7 +60,7 @@ const appointmentSchema = new mongoose_1.Schema({
                         // Time of day, like 1AM, 12PM, 10PM
                         enum: [
                             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-                            19, 20, 21, 22, 23, 24,
+                            19, 20, 21, 22, 23,
                         ],
                     },
                 },
